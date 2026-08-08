99952a117a41fd7da88f49eab71a5a9c32f47603
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-08-08 10:24:46 +0530

    Update stg_conversions.sql

--- models/staging/stg_conversions.sql
+++ models/staging/stg_conversions.sql
@@ -1,8 +1,12 @@
 select
+    conversion_id,
+    customer_id,
     campaign_id,
-    campaign_name,
-    channel,
-    cast(start_date as date) as start_date,
-    cast(end_date as date)   as end_date,
-    cast(budget as numeric(12,2)) as budget
-from {{ ref('campaigns') }}
+    cast(conversion_date as date) as conversion_date,
+    cast(revenue as numeric(10,2)) as revenue
+from {{ ref('conversions') }}
+where revenue is not null
+qualify row_number() over (
+    partition by conversion_id
+    order by conversion_date desc
+) = 1
