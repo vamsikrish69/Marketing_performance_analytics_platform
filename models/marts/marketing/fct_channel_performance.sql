c88c1d8334552ec3b956938684b0ac75a6863a2a
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-08-08 10:38:31 +0530

    Update fct_channel_performance.sql

--- models/marts/marketing/fct_channel_performance.sql
+++ models/marts/marketing/fct_channel_performance.sql
@@ -1,24 +1,9 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='conversion_id',
-        incremental_strategy='merge'
-    )
-}}
-
 select
-    ft.conversion_id,
-    ft.customer_id,
-    ft.first_touch_campaign_id,
-    lt.last_touch_campaign_id,
-    ft.conversion_date,
-    ft.revenue
-from {{ ref('int_attribution_first_touch') }} ft
-left join {{ ref('int_attribution_last_touch') }} lt
-    on ft.conversion_id = lt.conversion_id
-
-{% if is_incremental() %}
-where ft.conversion_date >= (
-    select dateadd('day', -3, max(conversion_date)) from {{ this }}
-)
-{% endif %}
+    c.channel,
+    sum(f.total_spend)   as total_spend,
+    sum(f.total_revenue) as total_revenue,
+    sum(f.total_revenue) / nullif(sum(f.total_spend), 0) as roas
+from {{ ref('fct_campaign_performance') }} f
+join {{ ref('stg_campaigns') }} c
+    on f.campaign_id = c.campaign_id
+group by 1
