3eb63108c17abed25b5708baea82bcaf08ecc8be
vamsikrish98 <vamsikrisshna06@gmail.com>
2026-08-08 10:24:02 +0530

    Modify stg_customers.sql to include new fields

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,12 +1,7 @@
 select
-    conversion_id,
     customer_id,
-    campaign_id,
-    cast(conversion_date as date) as conversion_date,
-    cast(revenue as numeric(10,2)) as revenue
-from {{ ref('conversions') }}
-where revenue is not null
-qualify row_number() over (
-    partition by conversion_id
-    order by conversion_date desc
-) = 1
+    segment,
+    lifecycle_stage,
+    cast(signup_date as date) as signup_date,
+    region
+from {{ ref('customers') }}
